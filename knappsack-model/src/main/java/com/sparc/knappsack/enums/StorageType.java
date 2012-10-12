f53a369528357f7eb966d21afbf8dc5a2ca1de89
Will Kennedy <will.kennedy@sparcedge.com>
2012-10-12 14:09:40 -0400

    Updated StorageType enum with remote flag

--- knappsack-model/src/main/java/com/sparc/knappsack/enums/StorageType.java
+++ knappsack-model/src/main/java/com/sparc/knappsack/enums/StorageType.java
@@ -2,16 +2,24 @@ package com.sparc.knappsack.enums;
 
 public enum StorageType {
     //Stored on the local server
-    LOCAL("storageType.local");
+    LOCAL("storageType.local", false);
 
+    //The message properties key for this StorageType
     private final String messageKey;
+    //Specifies if the StorageType is not the server running Knappsack
+    private final boolean remote;
 
-    private StorageType(String messageKey) {
+    private StorageType(String messageKey, boolean remote) {
         this.messageKey = messageKey;
+        this.remote = remote;
     }
 
     @SuppressWarnings("unused")
     public String getMessageKey() {
         return messageKey;
+    }
+
+    public boolean isRemote() {
+        return remote;
     }
 }
